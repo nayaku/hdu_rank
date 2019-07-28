9ab1758030c1f3888d55706e18bd540ba9abba8e
雪靡 <736248591@qq.com>
2019-07-28 10:07:51 +0800

    2019年7月28日

--- client/Vue.config.js
+++ client/Vue.config.js
@@ -1,15 +1,9 @@
-const plugins = ['@vue/babel-plugin-transform-vue-jsx']
-
 let devServer = (process.env.NODE_ENV === 'development') ? {
   devServer: {
     proxy: 'http://localhost:5000'
   }
 } : {}
 
-// 生产环境移除console
-if (process.env.NODE_ENV === 'production') {
-  plugins.push('transform-remove-console')
-}
 module.exports = {
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: false,
